548b7cee65e88a57423eb3f991cd2bde4aeffddf
gmheinrich <gmheinrich@gmail.com>
2021-05-16 17:16:18 -0300

    Assignment 3

--- authenticate.js
+++ authenticate.js
@@ -36,3 +36,14 @@ exports.jwtStrategy = passport.use(new JwtStrategy(opts,
     }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin === true) {
+        return next();
+    }
+    else { 
+        var err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    };
+};
