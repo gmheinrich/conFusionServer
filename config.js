43c2dcfe23e80ec2adb3708c03510a0739af87f3
gmheinrich <gmheinrich@gmail.com>
2021-05-23 16:25:13 -0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clienId: '1987487074740407',
+        clientSecret: '56a877d6f7075cdda76530dd4dd2b541'
+    }
 }
